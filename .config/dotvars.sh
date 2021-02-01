423ab27e5190de9ca553a4e699495c8602123682
Andres Gonzalez <1596691+in0ni@users.noreply.github.com>
2021-02-01 09:35:54 -0500

    dotvars: inital version

---
+++ .config/dotvars.sh
@@ -0,0 +1,108 @@
+#
+# THEME
+#
+theme=Flat-Remix-GTK-Green-Dark-Solid
+theme_icon=Tela-manjaro-dark
+theme_icon_variant=Tela-manjaro
+
+#
+# FONTS
+#
+font_sans="IBM Plex Sans"
+font_mono="IBM Plex Mono"
+font_icon="Font Awesome 5 Free"
+font_size=11
+font_size_m=10.5
+font_size_sm=10
+
+# transparency
+trnz_10=e5
+
+# typeface configuration
+hintstyle=hintfull
+rgba=rgb
+
+#
+# COLORS
+#
+
+#
+# -> theme-based colors
+
+theme_highlight=06a284
+
+theme_black=1b1d22
+theme_darkest=23252e
+theme_dark=272a34
+theme_mid=515967
+theme_light=b2b1b9
+theme_lightest=eeeeec
+theme_white=ffffff
+
+#
+# -> terminal
+
+# black (same as theme_black)
+term_color0=1b1d22
+term_color8=666675
+# red
+term_color1=e50072
+term_color9=ff87c3
+# green
+term_color2=45db00
+term_color10=8cffb9
+# yellow
+term_color3=ffec00
+term_color11=fff577
+# blue
+term_color4=3384ff
+term_color12=9fc5ff
+# magenta
+term_color5=cf78dc
+term_color13=da99e5
+# cyan
+term_color6=00efde
+term_color14=8cfff7
+# white
+term_color7=f2f2f2
+term_color15=ffffff
+
+# extended base 16
+term_color16=ff9f1c
+term_color17=8a4700
+term_color18=262840
+term_color19=898998
+term_color20=c1c1c8
+term_color21=e3e3e3
+
+#
+# -> gruvbox
+# NOTE: bg goes from muted (0) to full (4)
+gruv_dark_bg_0=7c6f64
+gruv_dark_bg_1=665c54
+gruv_dark_bg_2=504945
+gruv_dark_bg_3=3c3836
+gruv_dark_bg_4=282828
+gruv_dark_gray=928374
+gruv_dark_red=fb4934
+gruv_dark_green=b8bb26
+gruv_dark_yellow=fabd2f
+gruv_dark_blue=83a598
+gruv_dark_purple=d3869b
+gruv_dark_aqua=8ec07c
+gruv_dark_orange=fe8019
+
+gruv_light_bg_0=a89984
+gruv_light_bg_1=bdae93
+gruv_light_bg_2=d5c4a1
+gruv_light_bg_3=ebdbb2
+gruv_light_bg_4=f9f5d7
+gruv_light_gray=928374
+gruv_light_red=9d0006
+gruv_light_green=79740e
+gruv_light_yellow=b57614
+gruv_light_blue=076678
+gruv_light_purple=8f3f71
+gruv_light_aqua=427b58
+gruv_light_orange=d65d0e
+
