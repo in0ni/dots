a480f056ffbe289a89c83c4982e79d5dd3d9f473
Andrés González <1596691+in0ni@users.noreply.github.com>
2024-05-24 09:42:18 -0500

    koppa: new syntax %{}% and "!{}!", remove vim: ft= on all files

--- .config/xplr/init__.lua
+++ .config/xplr/init__.lua
@@ -1,7 +1,25 @@
 version = '0.21.7'
 
 xplr.config.general.panel_ui.default.border_type = "Thick"
-xplr.config.general.focus_ui.style.fg = { Rgb = {%{{theme_highlight_rgb}}} }
+xplr.config.general.panel_ui.default.border_style.fg = { Rgb = {"!{gruv_dark_1_rgb}!"} }
+
+-- main pane titles
+xplr.config.general.panel_ui.table.title.style.fg = { Rgb = {"!{gruv_light_0_h_rgb}!"} }
+xplr.config.general.panel_ui.selection.title.style.fg = { Rgb = {"!{gruv_light_0_h_rgb}!"} }
+
+-- secondary pane titles
+xplr.config.general.panel_ui.sort_and_filter.title.style.fg = { Rgb = {"!{gruv_light_2_rgb}!"} }
+xplr.config.general.panel_ui.input_and_logs.title.style.fg = { Rgb = {"!{gruv_light_2_rgb}!"} }
+xplr.config.general.panel_ui.help_menu.title.style.fg = { Rgb = {"!{gruv_light_2_rgb}!"} }
+
+-- secondary pane content
+xplr.config.general.table.header.style.fg = { Rgb = {"!{gruv_light_4_rgb}!"} }
+xplr.config.general.panel_ui.sort_and_filter.style.fg = { Rgb = {"!{gruv_light_4_rgb}!"} }
+xplr.config.general.panel_ui.help_menu.style.fg = { Rgb = {"!{gruv_light_4_rgb}!"} }
+
+-- active/selected items
+xplr.config.general.focus_ui.style.fg = { Rgb = {"!{theme_highlight_rgb}!"} }
+xplr.config.general.selection_ui.style.fg = { Rgb = {"!{theme_highlight_rgb}!"} }
 
 --
 -- key bindings
